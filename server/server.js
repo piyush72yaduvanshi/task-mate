0fbed4fa601f1978f03cc18151151d49351814ec
Piyush Yadav <151051765+piyush72yaduvanshi@users.noreply.github.com>
2025-04-13 17:24:14 +0530

    update cors in server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: ['http://localhost:5173'],
+        credentials: true
+      }
+));
 app.use(express.json());
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
